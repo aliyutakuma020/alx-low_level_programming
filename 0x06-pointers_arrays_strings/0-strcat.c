2414f7e23b5fc983af106909c5856e8ae16e5aac
aliyu <aliyutakuma020@gmail.com>
2023-09-20 14:45:51 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _strcat - Concatenates two strings.
+* @dest: Pointer to the destination string.
+* @src: Pointer to the source string.
+*
+* Return: Return (dest).
+*/
+char *_strcat(char *dest, char *src)
+{
+int dest_len = 0;
+int i;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+	dest[dest_len + i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Project: C programming tasks involving pointers, arrays, and strings. Requirements include coding style, specific compiler options, and various function implementations.
