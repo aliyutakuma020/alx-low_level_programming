c30d9bdf9c0234170eab2f438124b88a0fec3f13
aliyu <aliyutakuma020@gmail.com>
2023-09-21 12:55:03 -0700

    4-main.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array - Reverses an array of integers.
+ * @a: Pointer to the integer array to be reversed.
+ * @n: Number of elements in the array.
+ *
+ */
+void reverse_array(int *a, int n)
+{
+int len;
+int rev;
+
+	for (len = 0; len < n / 2; len++)
+	{
+	rev = a[len];
+	a[len] = a[n - len -1];
+	a[n - len -1] = rev;
+	}
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,5 @@
 #ifndef main_h
 #define main_h
-
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -14,5 +13,4 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 int _putchar(char c);
-
 #endif
