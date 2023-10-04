c511fdc8ed91e403f0b176898ef7b10e3a324b5d
aliyu <aliyutakuma020@gmail.com>
2023-10-04 13:55:06 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,7 +5,7 @@
  * str_concat - concatenates two strings.
  * @s1: First string.
  * @s2: second string.
- * return: return concat(string).
+ * return: return concat(*s1 and *s2).
  */
 char *str_concat(char *s1, char *s2)
 {
