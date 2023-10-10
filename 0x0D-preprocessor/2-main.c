d43c461e4249d1c10c65ee572511dcaacac29e60
aliyu <aliyutakuma020@gmail.com>
2023-10-10 09:39:58 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /**
- * main - Entry point of the program
- *
+ * main - Entry point of the program.
  * Description: This function prints the name of the source file it was
  * compiled from, followed by a newline.
  *
  * Return: Always 0 (Success)
  */
-int main(void)
+int main()
 {
-	
 	printf("%s\n", __FILE__);
 	return (0);
 }
