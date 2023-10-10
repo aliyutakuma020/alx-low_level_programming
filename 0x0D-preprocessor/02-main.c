0970e408e59aab4507d02be4aa7145116d6c851e
aliyu <aliyutakuma020@gmail.com>
2023-10-10 09:33:10 -0700

    V

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints the name of the source file it was
+ * compiled from, followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	
+	printf("%s\n", __FILE__);
+	return (0);
+}
