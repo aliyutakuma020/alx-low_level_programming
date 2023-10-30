5c11af1a0491fd51db8be9d475b92318015cc965
aliyu <aliyutakuma020@gmail.com>
2023-10-30 15:25:00 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC);
+	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (file_desc == -1)
 	{
 		return (-1);
