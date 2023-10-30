22a1d66bb512f561d4d049914054acac87559010
aliyu <aliyutakuma020@gmail.com>
2023-10-30 15:06:06 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -23,8 +23,11 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i]; i++);
-		bytes_written = write(file_desc, text_content, text_content[i]);
+		for (i = 0; text_content[i];)
+		{
+			i++;
+		}
+		bytes_written = write(file_desc, text_content, i);
 		if (bytes_written == -1)
 		{
 			close(file_desc);
